e243be0bb83e0762b4ec1dd88972feaac3516c80
GPplay <gybramllamasp@gmail.com>
2024-12-28 18:31:19 -0500

    creando rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_flutter/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,16 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Chat App',
+        initialRoute: 'loading',
+        routes: appRoutes);
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/material.dart';
+
 import 'package:chat_flutter/pages/chat_page.dart';
 import 'package:chat_flutter/pages/loading_page.dart';
 import 'package:chat_flutter/pages/login_page.dart';
 import 'package:chat_flutter/pages/register_page.dart';
 import 'package:chat_flutter/pages/usuario_page.dart';
 
-final Map<String, dynamic> appRoutes = {
+final Map<String, Widget Function(BuildContext)> appRoutes = {
   "usuarios": (_) => UsuarioPage(),
   "chat": (_) => ChatPage(),
   "login": (_) => LoginPage(),
