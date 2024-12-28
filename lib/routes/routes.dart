840ea61d0b418dca875d8fd7eb619ede25a75ed1
GPplay <gybramllamasp@gmail.com>
2024-12-28 18:21:55 -0500

    rutas

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,1 +1,13 @@
-final Map<String, dynamic> appRoutes = {};
+import 'package:chat_flutter/pages/chat_page.dart';
+import 'package:chat_flutter/pages/loading_page.dart';
+import 'package:chat_flutter/pages/login_page.dart';
+import 'package:chat_flutter/pages/register_page.dart';
+import 'package:chat_flutter/pages/usuario_page.dart';
+
+final Map<String, dynamic> appRoutes = {
+  "usuarios": (_) => UsuarioPage(),
+  "chat": (_) => ChatPage(),
+  "login": (_) => LoginPage(),
+  "loading": (_) => LoadingPage(),
+  "register": (_) => RegisterPage(),
+};
