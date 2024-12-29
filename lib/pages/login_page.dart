012fff6558c445fc0b291227266ca9142370620f
GPplay <gybramllamasp@gmail.com>
2024-12-28 19:34:42 -0500

    arreglando pagina login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,9 +6,87 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('login'),
+      backgroundColor: Color(0xffF2F2F2),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            _Logo(),
+            _Form(),
+            _Labels(),
+            Text(
+              'Términos y condiciones de uso',
+              style: TextStyle(fontWeight: FontWeight.w200),
+            )
+          ]),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 170,
+        margin: EdgeInsets.only(top: 50),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Image(image: AssetImage('assets/tag-logo.png')),
+              SizedBox(height: 20),
+              Text('Messenger',
+                  style: TextStyle(fontSize: 30, color: Colors.blue[600]))
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TextField(),
+          TextField(),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Ingresar'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('¿No tienes cuenta?',
+            style: TextStyle(
+                color: Colors.black54,
+                fontSize: 15,
+                fontWeight: FontWeight.w300)),
+        SizedBox(height: 10),
+        Text('Crea una ahora!',
+            style: TextStyle(
+                color: Colors.blue[600],
+                fontSize: 18,
+                fontWeight: FontWeight.bold))
+      ],
     );
   }
 }
