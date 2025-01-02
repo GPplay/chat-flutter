d3a2b32cecfaa1ed9eff3cc08f124636467fcd86
GPplay <gybramllamasp@gmail.com>
2025-01-02 13:13:41 -0500

    chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,8 +6,39 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('chat'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(children: <Widget>[
+          CircleAvatar(
+            child: Text('Te', style: TextStyle(fontSize: 12)),
+            backgroundColor: Colors.blue[100],
+            maxRadius: 14,
+          ),
+          SizedBox(height: 3),
+          Text('melisa', style: TextStyle(color: Colors.black87, fontSize: 12)),
+        ]),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+                child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (_, i) => Text('$i'),
+              reverse: true,
+            )),
+
+            Divider(height: 1),
+
+            //TODO: Caja de texto
+            Container(
+              color: Colors.white,
+              height: 100,
+            )
+          ],
+        ),
       ),
     );
   }
